d0979a9fc963dc0ae0c599d56727925efa97cd4e
Rivaldo-03 <contato.rivaldodahotmart@gmail.com>
2026-07-30 17:31:42 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { spawn } = require('child_process');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -48,5 +48,5 @@ app.get('/download', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
